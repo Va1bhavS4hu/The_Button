d1f7ec40aab1ec396efe5935ab06a5160db92d7b
vaibhavsahu-ux <vaibhavsahu50cs@gmail.com>
2025-07-29 18:22:02 +0530

    button click animation

--- main.js
+++ main.js
@@ -2,11 +2,21 @@ const button = document.querySelector("img");
 const h2 = document.querySelector("h2");
 count = 0
 
+function downclick() {
+	document.querySelector("img").src = "Button_Down.png"
+}
+
+function upclick() {
+	document.querySelector("img").src = "Button.png"
+}
+
 function handleclick() {
   count += 1
   h2.textContent = count
 }
 
+button.addEventListener("mousedown", downclick);
+button.addEventListener("mouseup", upclick);
 button.addEventListener("click", handleclick);
 
 
