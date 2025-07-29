b8cb8b702f18deb9df8697d1c9a4920a2a25f386
vaibhavsahu-ux <vaibhavsahu50cs@gmail.com>
2025-07-29 22:00:43 +0530

    added sound effects

--- main.js
+++ main.js
@@ -3,11 +3,13 @@ const h2 = document.querySelector("h2");
 count = 0
 
 function downclick() {
-	document.querySelector("img").src = "Button_Down.png"
+	document.querySelector("img").src = "Button_Down.png";
+	new Audio("click_down.mp3").play();
 }
 
 function upclick() {
-	document.querySelector("img").src = "Button.png"
+	document.querySelector("img").src = "Button.png";
+	new Audio("click_up.mp3").play();
 }
 
 function handleclick() {
